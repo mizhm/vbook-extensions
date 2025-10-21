eb4178ec0e8896f1eeb213ad935b5a54eb32158c
mizhm <minhnt141204@gmail.com>
2025-10-21 23:56:57 +0700

    fix: fix detail page for vozer ext

--- vozer/src/config.js
+++ vozer/src/config.js
@@ -1,9 +1,5 @@
 let BASE_URL = "https://vozer.io";
-// let BASE_UA = "PostmanRuntime/7.48.0";
-// let BASE_UA =
-// 	"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
-
-let BASE_UA = "demo";
+let BASE_UA = "ua";
 try {
 	if (CONFIG_URL) {
 		BASE_URL = CONFIG_URL;
