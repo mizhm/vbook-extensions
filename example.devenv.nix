91fd3c0cee107516095145502ad0545b0bac2810
mizhm <minhnt141204@gmail.com>
2025-10-28 16:02:35 +0700

    update plugin

---
+++ example.devenv.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  # # https://devenv.sh/packages/
+  # packages = [pkgs.git];
+  #
+  # languages.java = {
+  #   enable = true;
+  #   jdk.package = pkgs.jdk21.override {
+  #     enableJavaFX = true;
+  #     openjfx_jdk = pkgs.openjfx.override {withWebKit = true;};
+  #   };
+  # };
+}
--- vihentai/src/genre.js
+++ vihentai/src/genre.js
@@ -1,14 +1,13 @@
 load("config.js");
 
 function execute() {
-	const doc = fetch(BASE_URL).html();
-	console.log(doc);
-	const data = doc.select("ul[class='grid grid-cols-2'] > a").map((e) => {
-		return {
-			title: e.select("span").last().text(),
-			input: BASE_URL + e.attr("href"),
-			script: "genrecontent.js",
-		};
-	});
-	return Response.success(data);
+  const doc = fetch(BASE_URL).html();
+  const data = doc.select("ul[class='grid grid-cols-2'] > a").map((e) => {
+    return {
+      title: e.select("span").last().text(),
+      input: BASE_URL + e.attr("href"),
+      script: "genrecontent.js",
+    };
+  });
+  return Response.success(data);
 }
