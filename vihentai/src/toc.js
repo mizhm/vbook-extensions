d9ca1cfbf83c8b12df9c1ee395c178515696f3d1
mizhm <minhnt141204@gmail.com>
2025-10-28 17:04:44 +0700

    update toc

--- vihentai/src/toc.js
+++ vihentai/src/toc.js
@@ -3,15 +3,17 @@ load("config.js");
 function execute(url) {
 	const doc = fetch(url).html();
 
-	const data = doc
-		.select("ul[class='overflow-y-auto overflow-x-hidden'] a")
-		.map((e) => {
-			return {
-				name: e.select(".text-ellipsis").first().text(),
-				url: BASE_URL + e.attr("href"),
-				host: BASE_URL,
-			};
+	const list = doc.select("ul[class='overflow-y-auto overflow-x-hidden'] a");
+
+	const data = [];
+
+	for (let i = list.size() - 1; i >= 0; i--) {
+		data.push({
+			name: list.get(i).select(".text-ellipsis").first().text(),
+			url: BASE_URL + list.get(i).attr("href"),
+			host: BASE_URL,
 		});
+	}
 
 	return Response.success(data);
 }
