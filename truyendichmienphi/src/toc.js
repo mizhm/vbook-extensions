c54b35d821d8ce493a1f6db698fbdf65253ebd93
mizhm <minhnt141204@gmail.com>
2025-11-02 16:44:25 +0700

    fix(truyendichmienphi): fix chapter shuffle because pass wrong query

--- truyendichmienphi/src/toc.js
+++ truyendichmienphi/src/toc.js
@@ -4,18 +4,21 @@ load("util.js");
 function execute(url) {
 	const apiURL = getAPIURL(url);
 	const data = fetch(`${apiURL}/chapters`, {
-		limit: 99999,
-		page: 1,
-		sortBy: "chaper_number:asc",
+		queries: {
+			limit: 99999,
+			sortBy: "chapter_number:asc",
+		},
 	}).json();
 
-	return Response.success(
-		data.results.map((toc) => {
-			return {
-				name: `Chap ${toc.chapter_number}: ${toc.title}`,
-				url: `${apiURL}/chapter/${toc.chapter_number}`,
-				host: BASE_URL,
-			};
-		}),
-	);
+	const list = [];
+
+	data.results.forEach((toc) => {
+		list.push({
+			name: `Chap ${toc.chapter_number}: ${toc.title}`,
+			url: `${apiURL}/chapter/${toc.chapter_number}`,
+			host: BASE_URL,
+		});
+	});
+
+	return Response.success(list);
 }
