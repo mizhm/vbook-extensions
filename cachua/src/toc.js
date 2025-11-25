06c252478854fa6da5780d23e3474ed91ca7aaf2
mizhm <minhnt141204@gmail.com>
2025-11-25 19:37:32 +0700

    fix(cachua): fix toc cannot load

--- cachua/src/toc.js
+++ cachua/src/toc.js
@@ -1,23 +1,21 @@
 load("config.js");
 
 function execute(url) {
-	const htmlText = fetch(url).text();
+  const id = url.match(/\/page\/(\d+)/)[1];
+  const data = fetch(
+    `https://fanqienovel.com/api/reader/directory/detail?bookId=${id}`
+  ).json().data;
+  const list = [];
 
-	const regex = /window\.__INITIAL_STATE__\s*=\s*({.*});/;
-	const match = htmlText.match(regex);
-	const data = JSON.parse(match[1]);
+  data.chapterListWithVolume.forEach((volume) => {
+    volume.forEach((chapter) => {
+      list.push({
+        name: chapter.title,
+        url: `${API_URL}/content?item_id=${chapter.itemId}`,
+        host: BASE_URL,
+      });
+    });
+  });
 
-	const list = [];
-
-	data.page.chapterListWithVolume.forEach((volume) => {
-		volume.forEach((chapter) => {
-			list.push({
-				name: chapter.title,
-				url: `${API_URL}/content?item_id=${chapter.itemId}`,
-				host: BASE_URL,
-			});
-		});
-	});
-
-	return Response.success(list);
+  return Response.success(list);
 }
