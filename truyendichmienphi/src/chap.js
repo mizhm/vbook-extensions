677666c455a4307258690f5d3b22722884bf50d0
mizhm <minhnt141204@gmail.com>
2025-12-29 17:14:16 +0700

    fix(truyendichmienphi): fix chap content because website change logic

--- truyendichmienphi/src/chap.js
+++ truyendichmienphi/src/chap.js
@@ -2,14 +2,36 @@ load("config.js");
 load("crypto.js");
 
 function execute(url) {
-	const PASSPHRASE = "z4x8vog2a13vz4x8vog2a13v";
-	const response = fetch(url);
-	const decodedText = CryptoJS.AES.decrypt(
-		response.json().content,
-		PASSPHRASE,
-	).toString(CryptoJS.enc.Utf8);
-	const lines = decodedText.split("\n");
-	lines.reverse();
-	const content = lines.join("<br>");
-	return Response.success(content);
+  const text = fetch(getUrl(url)).text();
+  const regex = /\\"ver\\":\\"([a-f0-9]{32})\\"/;
+  const match = text.match(regex);
+
+  const PASSPHRASE = "z4x8vog2a13vz4x8vog2a13v124";
+
+  const response = fetch(url, {
+    queries: {
+      token: match[1],
+    },
+  });
+
+  const decodedText = CryptoJS.AES.decrypt(
+    response.json().content,
+    PASSPHRASE
+  ).toString(CryptoJS.enc.Utf8);
+
+  const lines = decodedText.split("\n");
+  lines.reverse();
+  const content = lines.join("<br>");
+  return Response.success(content);
+}
+
+function getUrl(apiUrl) {
+  let pageUrl = apiUrl.replace(/^(https?:\/\/)(api\.)/, "$1");
+
+  pageUrl = pageUrl
+    .replace("/api/novels/", "/truyen/")
+    .replace("/chapter/", "/chuong/");
+
+  pageUrl = pageUrl.replace(/\?.*$/, "");
+  return pageUrl;
 }
