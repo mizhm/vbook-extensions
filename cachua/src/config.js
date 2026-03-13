50bb9966a7c2ae3008c91738e0f05e05e21aadb2
mizhm <minhnt141204@gmail.com>
2026-03-13 17:08:38 +0700

    update fanqie

--- cachua/src/config.js
+++ cachua/src/config.js
@@ -1,6 +1,6 @@
 let BASE_URL = "https://fanqienovel.com";
-// const API_URL = "https://qkfqapi.vv9v.cn/api";
-const API_URL = "https://bk.yydjtc.cn/api";
+const API_URL = "https://qkfqapi.vv9v.cn/api";
+// const API_URL = "https://bk.yydjtc.cn/api";
 // const API_URL = "http://47.108.80.161:5005/api";
 
 try {
