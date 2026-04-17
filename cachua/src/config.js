b03e5ad65caa9a68160d6f656f7cde4150f3e5de
mizhm <minhnt141204@gmail.com>
2026-04-17 18:52:20 +0700

    fix cachua

--- cachua/src/config.js
+++ cachua/src/config.js
@@ -1,6 +1,7 @@
 let BASE_URL = "https://fanqienovel.com";
-const API_URL = "https://qkfqapi.vv9v.cn/api";
+// const API_URL = "https://qkfqapi.vv9v.cn/api";
 // const API_URL = "https://bk.yydjtc.cn/api";
+const API_URL = "http://101.35.133.34:5000/api";
 // const API_URL = "http://47.108.80.161:5005/api";
 
 try {
